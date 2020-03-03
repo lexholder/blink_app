508a8c450e9a20ea496fc431d5ad0c217aaa7ef7
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-03 12:02:12 -0500

    Created the different views for desktop and mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,18 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def dashboard
+    if request.headers["HTTP_USER_AGENT"].scan(/iPhone/).empty?
+      render :desktop_dashboard
+    else
+      render :mobile_dashboard
+    end
   end
 
   def home
+    if request.headers["HTTP_USER_AGENT"].scan(/iPhone/).empty?
+      render :desktop_home
+    else
+      render :mobile_home
+    end
   end
 end

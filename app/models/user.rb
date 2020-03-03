bf0e41a9a6e18e31f5e3bad1aa44a42534c72d1c
Erin Soggs <erinasoggs@gmail.com>
2020-03-03 17:07:12 -0500

    added default settings to every new user that is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  after_save :add_default_settings
   has_many :routines, dependent: :destroy
   has_one :setting, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def add_default_settings
+    Setting.create(user: self)
+  end
 end

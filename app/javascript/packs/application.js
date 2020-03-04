3ef6a26d7866f4f1fcbf9e1ffd78462075bbfc89
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-04 15:09:35 -0500

    Added the modals for exercise index page, with exercise details

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import { fillInModal } from "../components/modal_for_exercises";
+
+console.log("Hello from app/javascript/packs/application.js");
+window.fillInModal = fillInModal;
+

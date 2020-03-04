c3dd52c0b6407b30b7a34ae490f98e63e643985e
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-04 18:45:28 -0500

    Made the timer to run exercise and moved the navbar to be only on mobile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { fillInModal } from "../components/modal_for_exercises";
+import { exerciseTimer } from "../components/timer";
 
 console.log("Hello from app/javascript/packs/application.js");
 window.fillInModal = fillInModal;
+window.exerciseTimer = exerciseTimer;
 

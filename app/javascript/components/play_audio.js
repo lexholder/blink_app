cb4188bf68b4d155cc32e72aa86bedb30aede432
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-05 19:33:47 -0500

    Allowed clicking on other play buttons, previous audio stops and resets for a potential next play

--- app/javascript/components/play_audio.js
+++ app/javascript/components/play_audio.js
@@ -9,18 +9,30 @@ const setplayButtons = () => {
         if (event.currentTarget.classList.contains('playing')){
           event.currentTarget.classList.remove('playing');
           event.currentTarget.innerHTML = "Paused";
-          playButtonModal.classList.remove('playing');
-          playButtonModal.innerHTML = "Paused";
+          // playButtonModal.classList.remove('playing');
+          // playButtonModal.innerHTML = "Paused";
           audio.pause();
         } else {
+          if (window.audio && window.audio != audio){
+            window.audio.pause();
+            window.audio.currentTime = 0;
+            playButtons.forEach((button) => {
+              button.classList.remove('playing');
+              button.innerHTML = "Play";
+            })
+          }
+          window.audio = audio;
           event.currentTarget.classList.add('playing');
           event.currentTarget.innerHTML = "Playing";
-          playButtonModal.classList.add('playing');
-          playButtonModal.innerHTML = "Playing";
+          // playButtonModal.classList.add('playing');
+          // playButtonModal.innerHTML = "Playing";
           audio.play();
         }
       });
     });
+  }
+  if (playButtonModal) {
+
   }
 };
 

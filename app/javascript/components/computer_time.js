9370ed4364a4dfe89d135ef4b2571d95b00e177f
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-07 17:57:44 -0500

    Update le texte html

--- app/javascript/components/computer_time.js
+++ app/javascript/components/computer_time.js
@@ -1,7 +1,26 @@
 const updateOnHTML = () => {
-  return true;
+  const computerTime = document.getElementById("computer-time");
+  const nbMinTot = Number.parseInt(computerTime.dataset.computerTimeMinutes, 10) + 1;
+  computerTime.dataset.computerTimeMinutes = nbMinTot;
+  const nbHrs = Math.floor(nbMinTot / 60);
+  const nbMin = nbMinTot % 60;
+  let hrsText = "";
+  if (!(nbHrs === 0 && nbMin != 0)){
+    hrsText = `${nbHrs}h`;
+  }
+  let minText = "";
+  if (nbMin < 10 && nbMin != 0){
+    minText = "0";
+  }
+  minText = minText + `${nbMin}`;
+  if (nbHrs === 0){
+    minText = minText + "min";
+  }
+  const hrs = document.getElementById('computer-time-hrs');
+  const min = document.getElementById('computer-time-min');
+  hrs.innerText = hrsText;
+  min.innerText = minText;
 };
-
 
 const computerTime = () => {
   document.addEventListener("DOMContentLoaded", () => {
@@ -10,9 +29,9 @@ const computerTime = () => {
       fetch("/computer_times/update_one_min", {
         method: "PATCH"
       })
-      // .then(() => {
-      //   updateOnHTML();
-      // })
+      .then(() => {
+        updateOnHTML();
+      })
     }, 60000);
   });
 };

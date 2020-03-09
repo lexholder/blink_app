28a62713ea92144cb88a8ed0ffb0eb2d1c2f0129
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-09 11:57:28 -0400

    Generating today's computer time and routines when a user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  after_create :add_default_settings
+  after_create :add_default_settings, :create_todays_computer_time, :generate_todays_routines
   has_many :computer_times
   has_many :routines, dependent: :destroy
   has_many :exercises, through: :routines
@@ -8,7 +8,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
   def add_default_settings
     Setting.create(user: self)
+  end
+
+  def create_todays_computer_time
+    ComputerTime.create(user: self, date: Date.today, minutes: 0)
+  end
+
+  def generate_todays_routines
+    offset = rand(Exercise.count)
+    rand_exercise = Exercise.offset(offset).first
+    rep = Recommendation.recommend_repetitions_morning(self)
+    Routine.create!({user: self, exercise: rand_exercise, date: Date.today, time_of_day: "morning", completed: false, repetition: rep})
+    if Time.now.hour > 17
+      offset = rand(Exercise.count)
+      rand_exercise = Exercise.offset(offset).first
+      rep = Recommendation.recommend_repetitions_night(self)
+      Routine.create!({user: self, exercise: rand_exercise, date: Date.today, time_of_day: "night", completed: false, repetition: rep})
+    end
   end
 end

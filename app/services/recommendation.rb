e6069584bb65c6931e24f1a00849707c96e5a7e4
Erin Soggs <erinasoggs@gmail.com>
2020-03-09 11:31:21 -0400

    finished method for generating exercise reps

--- app/services/recommendation.rb
+++ app/services/recommendation.rb
@@ -1,11 +1,63 @@
 class Recommendation
 
   def self.recommend_repetitions_morning(user)
-    return 4
+    humidity = fetch_humidity(user)
+    temperature = fetch_temperature(user)
+    screen_time = user.computer_times.find { |c| c.date == Date.today.prev_day }
+    points = 0
+    if humidity > 50
+      points += 1
+    end
+    if temperature > 30
+      points += 1
+    end
+    if screen_time
+      if screen_time.minutes < 180
+        points += 2
+      elsif screen_time.minutes < 360
+        points += 1
+      end
+    else
+      points += 2
+    end
+    if points >= 3
+      rep = 4
+    elsif points >= 1
+      rep = 8
+    else
+      rep = 12
+    end
+    return rep
   end
 
   def self.recommend_repetitions_night(user)
-    return 4
+    humidity = fetch_humidity(user)
+    temperature = fetch_temperature(user)
+    screen_time = user.computer_times.find { |c| c.date == Date.today }
+    points = 0
+    if humidity > 50
+      points += 1
+    end
+    if temperature > 30
+      points += 1
+    end
+    if screen_time
+      if screen_time.minutes < 180
+        points += 2
+      elsif screen_time.minutes < 360
+        points += 1
+      end
+    else
+      points += 2
+    end
+    if points >= 3
+      rep = 4
+    elsif points >= 1
+      rep = 8
+    else
+      rep = 12
+    end
+    return rep
   end
 
   private
@@ -16,12 +68,12 @@ class Recommendation
     return JSON.parse(open(url).read)
   end
 
-  def self.fetch_humidity
-    fetch_weather_infos()["main"]["humidity"]
+  def self.fetch_humidity(user)
+    fetch_weather_infos(user)["main"]["humidity"]
   end
 
-  def self.fetch_temperature
-    fetch_weather_infos()["main"]["temp"]
+  def self.fetch_temperature(user)
+    fetch_weather_infos(user)["main"]["temp"]
   end
 
 end

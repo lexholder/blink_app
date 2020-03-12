58ecd7a065e17eaf8d0d7cf022ad43ac9606e96c
Erin Soggs <erinasoggs@gmail.com>
2020-03-12 14:00:34 -0400

    made necessary changes to make email work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "the-blink-app.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.asset_host = { host: "the-blink-app.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
